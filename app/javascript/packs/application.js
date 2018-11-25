4512fd38d280fffaa80a363b5f04b0bc5d34cc75
guillaume dutti <guillaume.dutti@live.fr>
2018-11-25 12:16:30 +0100

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,13 @@
+import "bootstrap";
+import Typed from 'typed.js';
+
+let options = {
+  strings: ["Référencement naturel", "Création de sites Web", "Google et Facebook Ads"],
+  typeSpeed: 40,
+  loop: true,
+  backDelay: 3000,
+  backSpeed: 20,
+  showCursor: false,
+}
+
+let typed = new Typed(".main-title", options);
