e8270c42c5b807621209e1c1c7c6448453e54ca8
guillaume dutti <guillaume.dutti@live.fr>
2018-12-04 10:36:38 +0100

    add pages + timeline

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,27 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :youtube]
+  skip_before_action :authenticate_user!, only: [:home, :parcours, :amazon, :creaweb, :seo, :sea, :youtube, :socialmedia]
 
   def home
   end
 
+  def parcours
+  end
+
+  def amazon
+  end
+
+  def creaweb
+  end
+
+  def seo
+  end
+
+  def sea
+  end
+
   def youtube
+  end
+
+  def socialmedia
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
+  get 'parcours', to: 'pages#parcours', as: :parcours
   get 'youtube', to: 'pages#youtube', as: :youtube
+  get 'amazon', to: 'pages#amazon', as: :amazon
+  get 'seo', to: 'pages#seo', as: :seo
+  get 'sea', to: 'pages#sea', as: :sea
+  get 'socialmedia', to: 'pages#socialmedia', as: :socialmedia
+  get 'creaweb', to: 'pages#creaweb', as: :creaweb
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
